cdbe7535a56a4e0206896f95b38073fac8ab0256
BHARATH2503 <bharathkanagu003@gmail.com>
2022-11-04 23:52:49 +0530

    login correction

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,80 +2,80 @@ import "../css/Contact.css"
 export default function Contact() {
     return (
 
-        <div class="contact3 py-5">
-            <div class="row no-gutters">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <div class="card-shadow">
+        <div className="contact3 py-5">
+            <div className="row no-gutters">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-6">
+                            <div className="card-shadow">
                                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqbTGHPgqTjDnIE8lBAEDtvlsjIMJOJg7mJw&usqp=CAU" alt="h"
-                                    class="img-container" />
+                                    className="img-container" />
                             </div>
                         </div>
-                        <div class="col-lg-6">
-                            <div class="contact-box ml-3">
-                                <h1 class="font-weight-light mt-2">Quick Contact</h1>
-                                <form class="mt-4" >
-                                    <div class="row">
-                                        <div class="col-lg-12">
-                                            <div class="form-group mt-2">
-                                                <input class="form-control" type="text" placeholder="Name" />
+                        <div className="col-lg-6">
+                            <div className="contact-box ml-3">
+                                <h1 className="font-weight-light mt-2">Quick Contact</h1>
+                                <form className="mt-4" >
+                                    <div className="row">
+                                        <div className="col-lg-12">
+                                            <div className="form-group mt-2">
+                                                <input className="form-control" type="text" placeholder="Name" />
                                             </div>
                                         </div>
-                                        <div class="col-lg-12">
-                                            <div class="form-group mt-2">
-                                                <input class="form-control" type="text" placeholder="City" />
+                                        <div className="col-lg-12">
+                                            <div className="form-group mt-2">
+                                                <input className="form-control" type="text" placeholder="City" />
                                             </div>
                                         </div>
 
-                                        <div class="col-lg-12">
-                                            <div class="form-group mt-2">
-                                                <textarea class="form-control" rows="3" placeholder="Whether the bus time match our time."></textarea>
+                                        <div className="col-lg-12">
+                                            <div className="form-group mt-2">
+                                                <textarea className="form-control" rows="3" placeholder="Whether the bus times match with our time or not"></textarea>
                                             </div>
                                         </div>
-                                        <div class="col-lg-12">
-                                            <button type="button" class="btn btn-danger-gradiant mt-3 text-white border-0 px-3 py-2"><span> SUBMIT</span></button>
+                                        <div className="col-lg-12">
+                                            <button type="button" className="btn btn-danger-gradiant mt-3 text-white border-0 px-3 py-2"><span> SUBMIT</span></button>
                                         </div>
                                     </div>
                                 </form>
                             </div>
                         </div>
-                        <div class="col-lg-12">
-                            <div class="card mt-4 border-0 mb-4">
-                                <div class="row">
-                                    <div class="col-lg-4 col-md-4">
-                                        <div class="card-body d-flex align-items-center c-detail pl-0">
-                                            <div class="mr-3 align-self-center">
+                        <div className="col-lg-12">
+                            <div className="card mt-4 border-0 mb-4">
+                                <div className="row">
+                                    <div className="col-lg-4 col-md-4">
+                                        <div className="card-body d-flex align-items-center c-detail pl-0">
+                                            <div className="mr-3 align-self-center">
                                                 <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/contact/icon1.png" alt="h" />
                                             </div>
-                                            <div class="">
-                                                <h6 class="font-weight-medium">Address</h6>
-                                                <p class="">Kongu Engineering College,
+                                            <div className="">
+                                                <h6 className="font-weight-medium">Address</h6>
+                                                <p className="">Kongu Engineering College,
                                                     <br /> Perundurai,Erode.</p>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="col-lg-4 col-md-4">
-                                        <div class="card-body d-flex align-items-center c-detail">
-                                            <div class="mr-3 align-self-center">
+                                    <div className="col-lg-4 col-md-4">
+                                        <div className="card-body d-flex align-items-center c-detail">
+                                            <div className="mr-3 align-self-center">
                                                 <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/contact/icon2.png" alt="h" />
                                             </div>
-                                            <div class="">
-                                                <h6 class="font-weight-medium">Phone</h6>
-                                                <p class="">251 546 9442
+                                            <div className="">
+                                                <h6 className="font-weight-medium">Phone</h6>
+                                                <p className="">251 546 9442
                                                     <br /> 630 446 8851</p>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="col-lg-4 col-md-4">
-                                        <div class="card-body d-flex align-items-center c-detail">
-                                            <div class="mr-3 align-self-center">
+                                    <div className="col-lg-4 col-md-4">
+                                        <div className="card-body d-flex align-items-center c-detail">
+                                            <div className="mr-3 align-self-center">
                                                 <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/contact/icon3.png" alt="h" />
                                             </div>
-                                            <div class="">
-                                                <h6 class="font-weight-medium">Email</h6>
-                                                <p class="">
-                                                    bustime@gmail.com
+                                            <div className="">
+                                                <h6 className="font-weight-medium">Email</h6>
+                                                <p className="">
+                                                     bustime@gmail.com
                                                 </p>
                                             </div>
                                         </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -31,16 +31,16 @@ const Login = () => {
           <h3 className="Auth-form-title">Admin Login</h3>
           <div className="Auth-form-content">
             {error && <Alert variant="danger">{error}</Alert>}
-            <Form.Label>Email</Form.Label>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label  className="mb-1">Email</Form.Label>
+            <Form.Group className="mb-1" controlId="formBasicEmail">
               <Form.Control autoFocus required
                 type="email"
                 placeholder="Email address"
                 onChange={(e) => setEmail(e.target.value)}
               />
             </Form.Group>
-            <Form.Label>Password</Form.Label>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label className="mb-1">Password</Form.Label>
+            <Form.Group className="mb-4" controlId="formBasicPassword">
               <Form.Control
                 type="password" required
                 placeholder="Password"
@@ -48,7 +48,7 @@ const Login = () => {
               />
             </Form.Group>
 
-            <div className="d-grid mt-5">
+            <div className="d-grid mt-2">
               <Button variant="primary" type="Submit">
                 Log In
               </Button>
